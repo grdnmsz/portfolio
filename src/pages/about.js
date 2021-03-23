795a9c3c274ab3035a9188ae4a9fb57685ea3987
Gordon <gordonmes@gmail.com>
2021-03-23 19:00:14 +0100

    moved text-color to global.css

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,7 @@ const About = ({ data }) => {
   return (
     <Layout>
       <SEO title="About me" />
-      <div className="py-12 text-gray-200">
+      <div className="py-12 ">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-left">
             <p className="mt-2 text-3xl text-center leading-8 font-extrabold tracking-tight sm:text-4xl">
