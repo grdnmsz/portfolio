417e1361ea16459797ef1f61862c11816eac7595
Gordon <gordonmes@gmail.com>
2021-03-24 05:06:21 +0100

    cleaning components imports

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Layout from "./layout";
+import SEO from "./seo";
+import Header from "./header";
+import ExperienceDetails from "./experienceDetails";
+import ExperienceCard from "./experienceCard";
+import Image from "./image";
+
+export { 
+  Layout, 
+  SEO, 
+  Header, 
+  ExperienceDetails, 
+  ExperienceCard, 
+  Image 
+};
