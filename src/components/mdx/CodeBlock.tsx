7ff640c811469008833e728f697002408d42781a
Gordon <gordonmes@gmail.com>
2021-03-26 21:37:09 +0100

    highlighting code syntax

---
+++ src/components/mdx/CodeBlock.tsx
@@ -0,0 +1,32 @@
+import React, { ReactElement } from "react";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import vsDark from "prism-react-renderer/themes/shadesOfPurple";
+
+const CodeBlock = ({ children, className }): ReactElement => {
+  const language = className ? className.replace(/language-/, "") : "";
+  defaultProps.theme = vsDark;
+  return (
+    <Highlight
+      {...defaultProps}
+      code={children}
+      language={language}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre
+          className={className + " overflow-x-scroll my-10 rounded-md max-w-3xl text-sm"}
+          style={{ ...style, padding: "20px" }}
+        >
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
+};
+
+export default CodeBlock;
