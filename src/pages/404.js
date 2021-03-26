de7df9e37f8cddd772f5a01b785d9423503b197f
Gordon <gordonmes@gmail.com>
2021-03-26 05:30:11 +0100

    MDXProvider, DesignSystem, non-permanent styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import { Layout, SEO } from "../components/";
 
 const NotFoundPage = () => (
@@ -7,6 +7,6 @@ const NotFoundPage = () => (
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
--- src/pages/about.js
+++ src/pages/about.js
@@ -7,50 +7,47 @@ const About = ({ data }) => {
   return (
     <Layout>
       <SEO title="About me" />
-      <div className="py-12 ">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="text-left">
-            <p className="mt-2 text-3xl text-center leading-8 font-extrabold tracking-tight sm:text-4xl">
-              About Me
-            </p>
-            <p className="mt-4 bg-headerColor rounded-xl py-6 px-6 text-sm md:text-base lg:mx-auto ">
-              I hold a{" "}
-              <span className="block text-orange-200 inline">MEng</span> in
-              Electronics, in addition to having experience in various domains,
-              from fundamental sciences and applied research, to professional
-              mobile development{" "}
-              <span role="img" aria-label="wave">
-                📱
-              </span>
-              .
-              <br />
-              <br className="" />2 years ago, I took my focus off
-              Computer Science to be more involved in creative projects such as
-              music composition and filmmaking. I’ve since been growing as a
-              Full Stack Engineer with
-              <span className="block text-orange-200 inline">
-                {" "}
-                React
-              </span> and{" "}
-              <span className="block text-orange-200 inline">Node.js</span>{" "}
-              expertise{" "}
-              <span role="img" aria-label="wave">
-                💻
-              </span>{" "}
-              , eager to take on{" "}
-              <span className="block text-orange-200 inline">
-                challenging missions
-              </span>{" "}
-              as well as to keep on improving my skills.
-            </p>
-          </div>
+      <div className="py-12 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="text-left">
+          <p className="mt-2 text-3xl text-center leading-8 font-extrabold tracking-tight sm:text-4xl">
+            About Me
+          </p>
+          <p className="mt-4 bg-headerColor rounded-xl py-6 px-6 text-sm md:text-base lg:mx-auto ">
+            I hold a <span className="block text-orange-200 inline">MEng</span>{" "}
+            in Electronics, in addition to having experience in various domains,
+            from fundamental sciences and applied research, to professional
+            mobile development{" "}
+            <span role="img" aria-label="wave">
+              📱
+            </span>
+            .
+            <br />
+            <br className="" />2 years ago, I took my focus off Computer Science
+            to be more involved in creative projects such as music composition
+            and filmmaking. I’ve since been growing as a Full Stack Engineer
+            with
+            <span className="block text-orange-200 inline">
+              {" "}
+              React
+            </span> and{" "}
+            <span className="block text-orange-200 inline">Node.js</span>{" "}
+            expertise{" "}
+            <span role="img" aria-label="wave">
+              💻
+            </span>{" "}
+            , eager to take on{" "}
+            <span className="block text-orange-200 inline">
+              challenging missions
+            </span>{" "}
+            as well as to keep on improving my skills.
+          </p>
+        </div>
 
-          <div className="mt-10">
-            <div className="space-y-16 md:space-y-0 md:grid md:grid-cols-4 md:gap-y-16">
-              {nodes.map((node, i) => {
-                return <ExperienceDetails key={i} node={node} />;
-              })}
-            </div>
+        <div className="mt-10">
+          <div className="space-y-16 md:space-y-0 md:grid md:grid-cols-4 md:gap-y-16">
+            {nodes.map((node, i) => {
+              return <ExperienceDetails key={i} node={node} />;
+            })}
           </div>
         </div>
       </div>
@@ -85,4 +82,4 @@ const staticQuery = (props) => (
   />
 );
 
-export default staticQuery
+export default staticQuery;
