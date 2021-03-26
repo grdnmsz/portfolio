3635ec869059bb8995eabe2739dcf68f28f9afdf
Gordon <gordonmes@gmail.com>
2021-03-26 05:30:11 +0100

    MDXProvider, DesignSystem, non-permanent styling

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import Layout from "./layout";
 import SEO from "./seo";
-import Header from "./header";
 import ExperienceDetails from "./experienceDetails";
 import ExperienceCard from "./experienceCard";
 import Image from "./image";
@@ -8,7 +7,6 @@ import Image from "./image";
 export { 
   Layout, 
   SEO, 
-  Header, 
   ExperienceDetails, 
   ExperienceCard, 
   Image 
