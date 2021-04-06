5f7c38f772ff4a2fe9d3ee0a4d4b6cc1eac796f9
Gordon <gordonmes@gmail.com>
2021-04-06 02:46:39 +0200

    css text animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,12 +36,12 @@ const IndexPage = ({
             <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-26">
               <div className="sm:text-center lg:text-left">
                 <h1 className="text-4xl tracking-tight font-extrabold sm:text-5xl md:text-6xl">
-                  <span className="block xl:inline">
+                  <span className="block animate-fade xl:inline">
                     Hi, <br />
                     I'm Gordon!
                   </span>
                 </h1>
-                <p className="mt-3 text-base sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 lg:mx-0">
+                <p className="mt-3 animate-fade text-base sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 lg:mx-0">
                   I’m a Software Engineer looking for new opportunities in Full
                   Stack development. Currently unemployed, I’m available and
                   motivated to take on creative and challenging missions.
