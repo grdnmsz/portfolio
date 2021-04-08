0162a725fbb13569eac6a3be0acd7ea01ce35a11
Gordon <gordonmes@gmail.com>
2021-04-08 05:18:12 +0200

    logo header, styling mobile tab pages

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className="relative flex items-center justify-between h-16">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <button
-              className="inline-flex items-center justify-center p-2 rounded text-gray-300 hover:text-white hover:bg-teal-800 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              className="inline-flex items-center justify-center p-2 rounded text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-expanded="false"
               onClick={() => setIsOn(!isOn)}
             >
@@ -48,15 +48,13 @@ const Header = () => {
             </button>
           </div>
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            {/* <div className="flex-shrink-0 flex items-center">
+            <div className="flex-shrink-0 flex items-center">
               <Link to={`/`}>
-                <img
-                  className="block h-10 w-auto"
-                  src={"https://i.imgur.com/NWNQfTS.png"}
-                  alt="PP"
-                />
+                {"gordon"}
+                <span className="text-teal-700">{"$>"}</span>
+                {"dev"}
               </Link>
-            </div> */}
+            </div>
             <div className="hidden sm:block sm:ml-6">
               <div className="flex space-x-4">
                 <Link
@@ -85,15 +83,15 @@ const Header = () => {
         <div className="px-2 pt-2 pb-3 space-y-1">
           <Link
             to={"/"}
-            className="hover:bg-teal-800 px-3 py-2 block rounded text-sm font-medium"
-            activeClassName="bg-teal-900 hover:bg-teal-800  px-3 py-2 block rounded text-sm font-medium"
+            className="px-3 py-2 block rounded text-sm font-medium"
+            activeClassName="bg-backgroundColor  px-3 py-2 block rounded text-sm font-medium"
           >
             Home
           </Link>
           <Link
             to={"/about"}
-            className=" hover:bg-teal-800 px-3 py-2 block rounded text-sm font-medium"
-            activeClassName="bg-teal-900 hover:bg-teal-800  px-3 py-2 block rounded text-sm font-medium"
+            className="px-3 py-2 block rounded text-sm font-medium"
+            activeClassName="bg-backgroundColor px-3 py-2 block rounded text-sm font-medium"
           >
             About
           </Link>
