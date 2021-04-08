0c31588eabf3ce92aea7e97fc39ae41923fe8f6a
Gordon <gordonmes@gmail.com>
2021-04-08 05:25:27 +0200

    logo size

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -50,9 +50,9 @@ const Header = () => {
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center">
               <Link to={`/`}>
-                {"gordon"}
+                <div className="text-xl">{"gordon"}
                 <span className="text-teal-700">{"$>"}</span>
-                {"dev"}
+                {"dev"}</div>
               </Link>
             </div>
             <div className="hidden sm:block sm:ml-6">
