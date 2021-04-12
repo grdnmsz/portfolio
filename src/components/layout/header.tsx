2cf5fa5a26c244ce1559449bc03686015b68fdce
Gordon <gordonmes@gmail.com>
2021-04-12 05:01:07 +0200

    responsive margin for logo website

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -50,7 +50,7 @@ const Header = () => {
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center ">
               <Link to={`/`}>
-                <div className="text-xl">
+                <div className="text-xl sm:mr-12">
                   {"gordon"}
                   <span className="text-teal-700">{"$>"}</span>
                   {"dev"}
