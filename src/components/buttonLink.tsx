f763b58c4533ff63b373730ad747ca8b857f1748
Gordon <gordonmes@gmail.com>
2021-04-27 11:07:18 +0200

    buttonLink refacto, cleaning dumb component (introBio)

---
+++ src/components/buttonLink.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type Props = {
+  text: string;
+  uri: string;
+};
+
+const ButtonLink = ({ text, uri }: Props) => (
+  <div className="rounded shadow">
+    <a
+      href={uri}
+      className="w-full flex items-center justify-center px-8 py-3 text-base font-medium rounded bg-headerColor hover:bg-teal-700 md:py-4 md:text-lg md:px-10"
+    >
+      {text}
+    </a>
+  </div>
+);
+
+export default ButtonLink;
