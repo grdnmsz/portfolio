c9076fe30274d9f7742f72a47d0b0747226a86b6
Gordon <gordonmes@gmail.com>
2021-04-28 23:38:00 +0200

    refining articles page and articleCard component

--- src/components/articleCard.tsx
+++ src/components/articleCard.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from "react";
 import { Link } from "gatsby";
+import { ButtonLink } from "./";
 
 export type ArticleCardProps = {
   frontmatter: {
@@ -16,13 +17,12 @@ const ArticleCard = (props: ArticleCardProps): ReactElement => {
   const { title, description, date } = props.frontmatter;
   const { slug } = props.fields;
   return (
-    <div className="max-w-xl">
-      <Link to={slug}>
-        <h1 className="sm:text-2xl text-xl">{title}</h1>
-        <div className="text-it">{description}</div>
-        <br />
-        {date}
-      </Link>
+    <div className="border-b max-w-lg py-8 ">
+      <h3 className="sm:text-2xl text-xl">{title}</h3>
+      <div className="text-gray-500 text-it">{description}</div>
+      <div className="border hover:text-teal-700 hover:border-teal-700 max-w-max mt-2 p-2 rounded">
+        <Link to={slug}>Read article </Link>
+      </div>
     </div>
   );
 };
