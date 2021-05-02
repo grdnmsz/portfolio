9cb472bb092b639aaefefd0385384af5a31629d9
Gordon <gordonmes@gmail.com>
2021-05-02 14:41:06 +0200

    first&quick article relecture, pre-code block theming and font size

---
+++ src/components/mdx/preCodeTheme.ts
@@ -0,0 +1,58 @@
+// Slightly overridden theme from prism-react-renderer dracula theme
+import { PrismTheme } from "prism-react-renderer"
+
+const theme: PrismTheme = {
+  plain: {
+    color: "#F8F8F2",
+    backgroundColor: "#282A36"
+  },
+  styles: [{
+    types: ["prolog", "constant", "builtin"],
+    style: {
+      color: "rgb(189, 147, 249)"
+    }
+  }, {
+    types: ["inserted", "function"],
+    style: {
+      color: "rgb(80, 250, 123)"
+    }
+  }, {
+    types: ["deleted"],
+    style: {
+      color: "rgb(255, 85, 85)"
+    }
+  }, {
+    types: ["changed"],
+    style: {
+      color: "rgb(255, 184, 108)"
+    }
+  }, {
+    types: ["punctuation", "symbol"],
+    style: {
+      color: "rgb(248, 248, 242)"
+    }
+  }, {
+    types: ["string", "char", "tag", "selector"],
+    style: {
+      color: "rgb(255, 121, 198)"
+    }
+  }, {
+    types: ["keyword", "variable"],
+    style: {
+      color: "rgb(189, 147, 249)",
+      fontWeight: "bold"
+    }
+  }, {
+    types: ["comment"],
+    style: {
+      color: "rgb(98, 114, 164)"
+    }
+  }, {
+    types: ["attr-name"],
+    style: {
+      color: "rgb(241, 250, 140)"
+    }
+  }]
+};
+
+export default theme;
