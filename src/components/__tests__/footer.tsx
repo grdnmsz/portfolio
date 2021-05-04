40a57ec2017e27d47cccc91da2a29c511b3a5b1f
Gordon <gordonmes@gmail.com>
2021-05-04 04:28:57 +0200

    update snapshots, fixing a jestconf typo (gatsby-* modules weren't transformed by jest), couldn't import using ES6 syntax in tests), folder structure reorganisation

--- src/components/__tests__/footer.tsx
+++ src/components/__tests__/footer.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 
-import Footer from '../layout/footer'
+import { Footer } from '../'
 
 describe('Footer', () => {
-  it('renders correctly', () => {
+  it('should renders correctly', () => {
     const tree = renderer.create(<Footer />).toJSON()
     expect(tree).toMatchSnapshot()
   })
