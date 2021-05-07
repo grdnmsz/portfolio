8fbc8185df501b2599fc6171cead0767cc7afc39
Gordon <gordonmes@gmail.com>
2021-05-07 22:17:29 +0200

    bundled fonts @fontsource/, csp headers for gtag

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,5 +4,9 @@
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+import '@fontsource/lato/400.css'
+import '@fontsource/lato/900.css'
+
+import '@fontsource/playfair-display/800.css'
+
 import './src/styles/global.css'
