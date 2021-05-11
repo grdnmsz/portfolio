4e6afaae9895d6a23c69f2a75d8ce5d48d09aeb4
Gordon <gordonmes@gmail.com>
2021-05-11 20:57:37 +0200

    404 change test

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import { Layout, SEO } from '../components'
 const NotFoundPage = (): ReactElement => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
+    <h1>404: Not Found, bad luck eh!</h1>
   </Layout>
 )
 
